bafa13ea07ef66e288da8d2a808ebe856e257041
Tan-Yifan <tanyifan@sjtu.edu.cn>
2024-01-07 21:43:48 +0800

    Fix

--- benchmarks/benchmark_serving.py
+++ benchmarks/benchmark_serving.py
@@ -40,15 +40,21 @@ def sample_requests(
     with open(dataset_path) as f:
         dataset = json.load(f)
     # Filter out the conversations with less than 2 turns.
-    dataset = [
-        data for data in dataset
-        if len(data["conversations"]) >= 2
-    ]
-    # Only keep the first two turns of each conversation.
-    dataset = [
-        (data["conversations"][0]["value"], data["conversations"][1]["value"])
-        for data in dataset
-    ]
+    if 'alpaca' in dataset_path:
+        dataset = [
+            (data['instruction'] + data["input"], data["output"])
+            for data in dataset
+        ]
+    else:
+        dataset = [
+            data for data in dataset
+            if len(data["conversations"]) >= 2
+        ]
+        # Only keep the first two turns of each conversation.
+        dataset = [
+            (data["conversations"][0]["value"], data["conversations"][1]["value"])
+            for data in dataset
+        ]
 
     # Tokenize the prompts and completions.
     prompts = [prompt for prompt, _ in dataset]
