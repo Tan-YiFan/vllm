8e792a247ad8373b6e85cfd278f36077ab1bb6d7
Tan-Yifan <tanyifan@sjtu.edu.cn>
2024-01-10 16:12:40 +0800

    Update

--- benchmarks/launch.sh
+++ benchmarks/launch.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export HF_HOME=$(pwd)/huggingface
-python3 -m vllm.entrypoints.api_server --model facebook/opt-30b --swap-space 60 --disable-log-requests
+python3 -m vllm.entrypoints.api_server --model facebook/opt-30b --swap-space 30 --disable-log-requests
 #python3 -m vllm.entrypoints.api_server --model facebook/opt-13b --swap-space 60 --disable-log-requests --max-num-seqs=1024 --max-num-batched-tokens=8192
